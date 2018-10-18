8106aabb3e4da177a1ad27bb3ec870d46a03ed2f
Alex Mos <alexander.mospan@gmail.com>
2018-10-18 22:38:26 +0300

    init

---
+++ freestyle/slice.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//numbers := []int{0, 1, 2, 3, 4, 5}
+	//fmt.Println(numbers[1:4])
+
+	var user map[string]bool = map[string]bool{
+		"firstName": true,
+		"lastName": false,
+	}
+
+	if _, keyExist := user["lastName"]; keyExist {
+		fmt.Println(true)
+	} else {
+		fmt.Println(false)
+	}
+
+	if _, keyExist := user["pastName"]; keyExist {
+		fmt.Println(true)
+	} else {
+		fmt.Println(false)
+	}
+
+	//fmt.Println(user["firstName"])
+	//fmt.Println(user["lastName"])
+	//fmt.Println(user["pastName"])
+}
+
