90091078a9c9fb605e930bf2311a4bcd30de6bee
Alex Mos <alexander.mospan@gmail.com>
2018-11-18 23:56:21 +0300

    test for uniq

--- coursera/week_1/string_uniq/main.go
+++ coursera/week_1/string_uniq/main.go
@@ -4,21 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"io"
 )
 
-func main() {
-	in := bufio.NewScanner(os.Stdin)
+func uniq(input io.Reader, output io.Writer) error {
+	in := bufio.NewScanner(input) // создаём новый сканнер из стандартного ввода
 	var prev string
-
 	for in.Scan() {
 		txt := in.Text()
 		if txt == prev {
 			continue
 		}
 		if txt < prev {
-			panic("string is not sorted")
+			return fmt.Errorf("file is not sorted")
 		}
 		prev = txt
-		fmt.Println(txt)
+		fmt.Fprintln(output, txt) // Принт, который принимает на вход io.Reader
+	}
+	return nil
+}
+
+func main() {
+	err := uniq(os.Stdin, os.Stdout)
+	if err != nil {
+		panic(err.Error())
 	}
 }
---
+++ coursera/week_1/string_uniq/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	//"fmt"
+	"strings"
+	"bytes"
+	"testing"
+)
+
+var testOk = `1
+2
+2
+3
+3
+3
+4
+5`
+
+var testOkResult = `1
+2
+3
+4
+5
+`
+
+func TestOk(t *testing.T) {
+	in := bufio.NewReader(strings.NewReader(testOk))
+	out := new(bytes.Buffer)
+	err := uniq(in, out)
+	if err != nil {
+		t.Errorf("test for OK failed – error")
+	}
+	result := out.String()
+	if out.String() != testOkResult {
+		t.Errorf("test for OK failed – results not match\ngot:\n%v\nexpected:\n%v", result, testOkResult)
+	}
+}
+
+var testFail = `1
+2
+1`
+
+func TestForError(t *testing.T) {
+	in := bufio.NewReader(strings.NewReader(testFail))
+	out := new(bytes.Buffer)
+	err := uniq(in, out)
+	if err == nil {
+		t.Errorf("test for Error failed – error\ngot:\n%v", err)
+	}
+}
